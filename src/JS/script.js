8e87231726d19d91b77f2dce4c12fa2361de0816
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-15 11:22:08 +0530

    30 tasks

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,231 +1,139 @@
-// 11.Introducing Else Statements
-function testElse(val) {
-    let result = "";
+// 21.Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
     // Only change code below this line
   
-    if (val > 5) {
-      result = "Bigger than 5";
-    }
+  if( a<0 ||b<0)
+  return;
   
-    else  {
-      result = "5 or Smaller";
-    }
+    // Only change code above this line
   
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
+
+//   22.Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+if (card==2 ||card ==3 || card==4 || card==5 ||card==6)
+{
+  count++;
+  
+}
+else if(card==10 ||card =='J' || card=='Q' || card=='K' ||card=='A')
+{
+  count --;
+}
+else if(card==7 ||card ==8 || card==9 )
+{
+  count=count;
+}
+
+  let ans=count>0? count+" "+"Bet": count+" "+"Hold";
+  return ans;
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+//  23.Build JavaScript Objects
+const myDog = {
+    // Only change code below this line
+  
+   "name": "Ford",
+    legs: 5,
+    tails: 2,
+    friends:[]
+    // Only change code above this line
+  };
+
+//   24.Accessing Object Properties with Dot Notation
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
+
+//   25.Accessing Object Properties with Bracket Notation
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line
+
+//   26.Accessing Object Properties with Variables
+/ Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+// 27.Updating Object Properties
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  myDog.name="Happy Coder";
+
+//   28.Add New Properties to a JavaScript Object
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "bow-wow";
+
+//   29.Delete Properties from a JavaScript Object
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
+
+//   30.Using Objects for Lookups
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    const lookup={
+      "alpha":"Adams",
+      "bravo":"Boston",
+      "charlie": "Chicago",
+      "delta":"Denver",
+       "echo":"Easy",
+       "foxtrot":"Frank"
+    }
+    
+  result=lookup[val];
     // Only change code above this line
     return result;
   }
   
-  testElse(4);
-
-//   12.Introducing Else If Statements
-function testElseIf(val) {
-    if (val > 10) {
-      return "Greater than 10";
-    }
-  
-    else if (val < 5) {
-      return "Smaller than 5";
-    }
-     else{return "Between 5 and 10";}
-    
-  }
-  
-  testElseIf(7);
-
-//   13.Logical Order in If Else Statements
-function orderMyLogic(val) {
-    if (val < 5) {
-      return "Less than 5";
-    } else if (val < 10) {
-      return "Less than 10";
-    } else {
-      return "Greater than or equal to 10";
-    }
-  }
-  
-  orderMyLogic(7);
-
-
-//   14.Chaining If Else Statements
-function testSize(num) {
-    // Only change code below this line
-  if (num < 5) {
-    return "Tiny";
-  } else if (num < 10) {
-    return "Small";
-  } else if (num < 15) {
-    return "Medium";
-  
-  }
-  else if (num < 20) {
-    return "Large";
-  
-  }
-  
-   else {
-    return "Huge";
-  }
-  
-    
-    // Only change code above this line
-  }
-  
-  testSize(7);
-
-//   15.Golf Code
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, strokes) {
-  // Only change code below this line
-   if(strokes==1)
-   {
-     return "Hole-in-one!";
-   }
-   else if(strokes<=par - 2)
-   {
-     return "Eagle";
-   }
-    else if(strokes==par - 1)
-   {
-     return "Birdie";
-   }
-   else if(strokes==par)
-   {
-     return "Par";
-   }
-   else if(strokes==par+ 1)
-   {
-     return "Bogey";
-   }
-   else if(strokes==par + 2)
-   {
-     return "Double Bogey";
-   }
-  
-  else{
-   return "Go Home!";
-  }
-  
-  // Only change code above this line
-}
-
-golfScore(5, 4);
- 
-// 16.Selecting from Many Options with Switch Statements
-function caseInSwitch(val) {
-    let answer = "";
-    // Only change code below this line
-  switch (val) {
-    case 1:
-      answer="alpha";
-      break;
-    case 2:
-      answer="beta";
-      break;
-    case 3:
-      answer="gamma";
-      break;
-    case 4:
-      answer="delta";
-      break;
-  }
-  
-  
-    // Only change code above this line
-    return answer;
-  }
-  
-  caseInSwitch(1);
-
-//   17.Adding a Default Option in Switch Statements
-function switchOfStuff(val) {
-    let answer = "";
-    // Only change code below this line
-  switch (val) {
-    case "a":
-      answer ="apple";
-      break;
-    case "b":
-     answer ="bird"
-      break;
-    case "c":
-      answer ="cat"
-      break; 
-  
-    default:
-      answer ="stuff"
-      break;
-  }
-  
-  
-    // Only change code above this line
-    return answer;
-  }
-  
-  switchOfStuff(1);
-
-//   18.Multiple Identical Options in Switch Statements
-function sequentialSizes(val) {
-    let answer = "";
-    // Only change code below this line
-  switch (val) {
-    case 1:
-    case 2:
-    case 3:
-      answer = "Low";
-      break;
-    case 4:
-    case 5:
-    case 6:
-    answer="Mid";
-    break;
-    case 7:
-    case 8:
-    case 9:
-    answer="High";
-  
-  }
-  
-  
-    // Only change code above this line
-    return answer;
-  }
-  
-  sequentialSizes(1);
-
-//   19.Replacing If Else Chains with Switch
-function chainToSwitch(val) {
-    let answer = "";
-    // Only change code below this line
-  switch (val)
-  {
-    case "bob":
-       answer = "Marley"; break;  
-    case 42:
-       answer = "The Answer"; break; 
-    case 1:
-       answer = "There is no #1"; break;  
-     case 99:
-       answer = "Missed me by this much!"; break;  
-      case 7:
-       answer = "Ate Nine"; break;       
-  
-  
-  }
-    
-  
-    // Only change code above this line
-    return answer;
-  }
-  
-  chainToSwitch(7);
-
-//   20.Returning Boolean Values from Functions
-function isLess(a, b) {
-    // Only change code below this line
-    return (a < b) ;
-    
-    // Only change code above this line
-  }
-  
-  isLess(10, 15);
+  phoneticLookup("charlie");
